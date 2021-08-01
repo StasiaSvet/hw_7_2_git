3e29a2822fd7a1e75ace79fe82d3092fd19752cc
StessyT <wildorhid@gmail.com>
2021-08-01 11:15:22 +0300

    Updated build.gradle

---
+++ src/test/java/guru/qa/FirstTest.java
@@ -0,0 +1,15 @@
+package guru.qa;
+
+import org.junit.jupiter.api.Test;
+
+public class FirstTest {
+
+    @Test
+    void firstTest() {
+        System.out.println("Hello, world");
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
